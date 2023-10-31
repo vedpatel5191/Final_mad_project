28c5ca352350865cb848837d580da71a701b8fdb
vedpatel5191 <ved2889@gmail.com>
2023-10-31 23:32:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_project/QuestionModel.kt
@@ -0,0 +1,8 @@
+package com.example.mad_project
+
+data class QuestionModel(val question:String,
+                         val option1:String,
+                         val option2:String,
+                         val option3:String,
+                         val option4:String,
+                         val answer:String)
